00cd086dbf0592da7b4f040f1dfb65725c14747e
anbanshayan <anbanshayan3@gmail.com>
2024-09-01 23:17:17 +0530

    python1

---
+++ first.py
@@ -0,0 +1,16 @@
+print("Hello World!!")
+#You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+if 5>2:
+    print("Five is greater than two")
+    print("5 is greater than two")
+
+x = 5
+y = "Anbashayan"
+
+print(x)
+print(y)
+
+z = str(3)
+print(z)
+
+print(type(z))
---
+++ second.py
@@ -0,0 +1,8 @@
+#01st September 2024
+
+a = 4
+A = "UCSC"
+# letters are case sensitive
+
+#variable can be initlized at a time
+
