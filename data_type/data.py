8c67bf3712ee75c8a0b0be6c3306b96927673e96
anbanshayan <anbanshayan3@gmail.com>
2024-10-12 01:57:09 +0530

    python_data_type_update_till_dict

--- data_type/data.py
+++ data_type/data.py
@@ -1,2 +1,23 @@
 x =12
 print(type(x))
+#output => <class 'int'>
+
+y = 20.2
+print(type(y))
+#output =><class 'float'>
+
+greeting = "Welcome freshers!!"
+print(type(greeting))
+#<class 'str'>
+
+degree_class = ["First class", "Second upper", "Second lower"," General"]
+print(type(degree_class))
+
+district  = ('Jaffna','Kilinochi','Mannar','Mullaitivu','Vavuniya')
+print(type(district))
+
+prime_number = {2,3,5,7,11}
+print(type(prime_number))
+
+capital = {"UAE":"Abu Dhabi","Sri Lanka":"Colombo"}
+print(type(capital))
