c361e7c03bef276cc5270305575022a5cce34b47
anbanshayan <anbanshayan3@gmail.com>
2024-10-11 23:58:32 +0530

    Saraswathy Pooja Special

---
+++ variables/variable1.py
@@ -0,0 +1,11 @@
+#Assigning multiple variables with multiple variables
+
+x ,y,z = "Banana","Apple","Grapes"
+print(x)
+print(y)
+print(z)
+
+#Assigning multples values for one variable
+j = 8,9,5
+print(j)
+#it simply creates tuple
