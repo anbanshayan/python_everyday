b78b70d2d68f5146cac8f5ed9d2ebcef85584107
anbanshayan <anbanshayan3@gmail.com>
2024-10-14 15:58:10 +0530

    self intitated

---
+++ object_class/object1.py
@@ -0,0 +1,19 @@
+#intrdouction of class
+class myClass:
+    x = 5
+
+obj1 = myClass()
+print(obj1.x)
+
+#Above ones are simple form of class and object
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+person1 = Person("Anbashayan",24)
+person2 = Person("AKugananthi",49)
+
+print(person1.name,person1.age)
+print(person2.name,person2.age)
