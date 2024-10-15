4a19063404e3fb996629c836b635290e0c578c12
anbanshayan <anbanshayan3@gmail.com>
2024-10-15 23:59:58 +0530

    klnj

---
+++ object_class/object4.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self,name,age):
+        self.fname = fname
+        self.lname = lname
+
+    def printName(self):
+        print(self.fname,self.lname)
+    
+x = person("Anban","Shayan")
